be7e69b0ffd3b574fbe2ad6e25e3b6b1a7a6105c
Tim Milligan <milamber@gmail.com>
2012-10-26 07:20:55 -0400

    Switched from wpdb to get/update option

--- copy_seed.php
+++ copy_seed.php
@@ -1,11 +1,9 @@
 <?php
 /*
-
 Name: copy_skin.php
 Author: Tim Milligan
 Description: Copies the seed data from one skin to another.  You can then generate a zip file for the new skin and distribute it normally.
-Version: 1.0
-
+Version: 2.0
 */
 
 // Thesis 2 skin you're trying to generate the seed for:
@@ -22,17 +20,7 @@ define('WP_DEBUG', false);
 require('wp-blog-header.php');
 
 foreach ($items as $item) {
-	$query = $wpdb->prepare('SELECT option_value FROM %3$s WHERE option_name = \'%1$s_%2$s\'', $original_skin, $item, $wpdb->options);
-	//echo $query . "<br />\n";
-	$seed[$item] = $wpdb->get_var($query);
-
-	//fix potential serialization issues
-	if(is_serialized($seed[$item])) {
-		$seed[$item] = preg_replace('!s:(\d+):"(.*?)";!e', "'s:'.strlen('$2').':\"$2\";'", $seed[$item]);
-		
-	}
-	$query = $wpdb->prepare('REPLACE INTO %4$s (option_name, option_value) VALUES (\'%1$s_%2$s\', \'%3$s\')', $new_skin, $item, $seed[$item], $wpdb->options);
-	//echo $query . "<br />\n";
-	$wpdb->query($query);
+	$seed[$item] = get_option(sprintf('%s_%s', $original_skin, $item));
+	update_option(sprintf('%s_%s', $new_skin, $item), $seed[$item]);
 }
-echo "$new_skin seeded with $original_skin data.";
+echo "[$new_skin] seeded with [$original_skin] data.";
